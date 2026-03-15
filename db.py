dd7140c9540a29f68cc0d935ea5948871f5f8b66
renato-sd-bautista-jr <bautistarenato017@gmail.com>
2026-03-15 23:20:56 +0800

    railway test hosting test db condition 1

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ def get_db_connection():
         return pymysql.connect(
             host=os.getenv("MYSQLHOST_PUBLIC") or "maglev.proxy.rlwy.net",
             user=os.getenv("MYSQLUSER") or "root",
-            password=os.getenv("MYSQLPASSWORD"),
+            password=os.getenv("MYSQLPASSWORD") or "mYhPJsfLwPUcVMYfHeoWOpFSmUXXfFIG",
             database=os.getenv("MYSQLDATABASE") or "railway",
             port=int(os.getenv("MYSQLPORT") or 48540),
             cursorclass=pymysql.cursors.DictCursor
