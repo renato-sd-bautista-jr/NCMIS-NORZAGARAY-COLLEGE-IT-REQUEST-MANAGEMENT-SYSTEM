b2425f739ae30430561f0cb1a33599eb02e7f034
renato-sd-bautista-jr <bautistarenato017@gmail.com>
2026-03-15 21:48:31 +0800

    railway test hosting 1

--- db.py
+++ db.py
@@ -1,10 +1,29 @@
-import pymysql
+import os,pymysql
+
+# def get_db_connection():
+#     return pymysql.connect(
+#         host='localhost',
+#         user='root',
+#         password='',
+#         database='ncmis',
+#        cursorclass=pymysql.cursors.DictCursor  # always returns dict rows
+#     )
+
+
 
 def get_db_connection():
+
+    host = os.getenv("MYSQLHOST", "localhost")
+    user = os.getenv("MYSQLUSER", "root")
+    password = os.getenv("MYSQLPASSWORD", "")
+    database = os.getenv("MYSQLDATABASE", "ncmis")
+    port = int(os.getenv("MYSQLPORT", 3306))
+
     return pymysql.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='ncmis',
-       cursorclass=pymysql.cursors.DictCursor  # always returns dict rows
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+        port=port,
+        cursorclass=pymysql.cursors.DictCursor
     )
