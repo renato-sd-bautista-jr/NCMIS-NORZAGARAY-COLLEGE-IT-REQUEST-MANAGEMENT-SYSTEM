c16e03342265703aaf0a0ea14779914007cf0c18
renato-sd-bautista-jr <bautistarenato017@gmail.com>
2026-03-15 23:35:04 +0800

    railway test hosting test db condition no conditional option 2

--- db.py
+++ db.py
@@ -1,12 +1,39 @@
 import pymysql
 import os
 
-DATABASE_URL = os.getenv("MYSQL_PUBLIC_URL")
+def get_db_connection():
+    try:
+        # If running on Railway
+        if os.getenv("MYSQLHOST"):
+            conn = pymysql.connect(
+                host=os.getenv("maglev.proxy.rlwy.net"),
+                user=os.getenv("root"),
+                password=os.getenv("mYhPJsfLwPUcVMYfHeoWOpFSmUXXfFIG"),
+                database=os.getenv("railway"),
+                port=int(os.getenv("48540")),
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            print("Connected to Railway database")
 
-conn = pymysql.connect(
-    host="maglev.proxy.rlwy.net",
-    user="root",
-    password="yourpassword",
-    database="railway",
-    port=48540
-)
+        # If running locally (XAMPP)
+        else:
+            conn = pymysql.connect(
+                host="localhost",
+                user="root",
+                password="",
+                database="ncmis",
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            print("Connected to local XAMPP database")
+
+        return conn
+
+    except Exception as e:
+        print(f"Database connection error: {e}")
+        raise
+    #conn = pymysql.connect(
+            #host="maglev.proxy.rlwy.net",
+            #user="root",
+            #password="mYhPJsfLwPUcVMYfHeoWOpFSmUXXfFIG",
+            #database="railway",
+            #port=48540
